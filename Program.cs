bd2b734b2dc96e4a7566616ec459b88991b4fc44
Corey T Cox <coxcorey19@gmail.com>
2020-05-12 11:05:56 -0400

    added line to welcome all

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bienvenidos a Dia 2");
         }
     }
 }
